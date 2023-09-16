ac49c5ccb64cc3986c4d756e473dc110bb76abf6
Kevin Vantus <kevinvantus@gmail.com>
2023-09-16 02:37:44 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  const todos = [];
+
+  function submit() {
+    console.log("form submitted");
+  }
+</script>
+
+<main class="container">
+  <div class="card">
+    <form>
+      <!-- <form on:submit={submit}> -->
+      <div class="u-flex u-gap-12">
+        <input
+          type="text"
+          class="input-text u-text-start"
+          placeholder="Enter todo"
+        />
+        <button class="button" on:click={submit}>
+          <span class="icon-plus" />
+          <span class="text">Add</span></button
+        >
+      </div>
+    </form>
+
+    <ul class="list u-text-start u-margin-block-start-32">
+      <li>
+        <input type="checkbox" id="" class="input-check" />
+        <label for="" />
+      </li>
+    </ul>
+  </div>
+</main>
+
+<style>
+  .container {
+    max-width: 35rem;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "@appwrite.io/pink-icons";
+import "@appwrite.io/pink";
+import "./app.css";
+import App from "./App.svelte";
+
+const app = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
